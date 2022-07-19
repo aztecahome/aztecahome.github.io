be2e38474347f7b92f4844fcbe5a1e2f2ab38504
Xavier Ruiz <xruiz@bu.edu>
2022-07-19 10:24:59 -0400

    Updates

---
+++ assets/index.9af5bf23.js
@@ -0,0 +1,124 @@
+const G=function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))i(a);new MutationObserver(a=>{for(const r of a)if(r.type==="childList")for(const c of r.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function t(a){const r={};return a.integrity&&(r.integrity=a.integrity),a.referrerpolicy&&(r.referrerPolicy=a.referrerpolicy),a.crossorigin==="use-credentials"?r.credentials="include":a.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(a){if(a.ep)return;a.ep=!0;const r=t(a);fetch(a.href,r)}};G();function g(){}function D(e){return e()}function U(){return Object.create(null)}function E(e){e.forEach(D)}function V(e){return typeof e=="function"}function M(e,s){return e!=e?s==s:e!==s||e&&typeof e=="object"||typeof e=="function"}function J(e){return Object.keys(e).length===0}function w(e,s){e.appendChild(s)}function p(e,s,t){e.insertBefore(s,t||null)}function n(e){e.parentNode.removeChild(e)}function f(e){return document.createElement(e)}function K(e){return document.createTextNode(e)}function A(){return K(" ")}function o(e,s,t){t==null?e.removeAttribute(s):e.getAttribute(s)!==t&&e.setAttribute(s,t)}function Q(e){return Array.from(e.childNodes)}let N;function S(e){N=e}const _=[],Z=[],H=[],q=[],X=Promise.resolve();let j=!1;function ee(){j||(j=!0,X.then(Y))}function I(e){H.push(e)}const R=new Set;let $=0;function Y(){const e=N;do{for(;$<_.length;){const s=_[$];$++,S(s),se(s.$$)}for(S(null),_.length=0,$=0;Z.length;)Z.pop()();for(let s=0;s<H.length;s+=1){const t=H[s];R.has(t)||(R.add(t),t())}H.length=0}while(_.length);for(;q.length;)q.pop()();j=!1,R.clear(),S(e)}function se(e){if(e.fragment!==null){e.update(),E(e.before_update);const s=e.dirty;e.dirty=[-1],e.fragment&&e.fragment.p(e.ctx,s),e.after_update.forEach(I)}}const C=new Set;let te;function P(e,s){e&&e.i&&(C.delete(e),e.i(s))}function B(e,s,t,i){if(e&&e.o){if(C.has(e))return;C.add(e),te.c.push(()=>{C.delete(e),i&&(t&&e.d(1),i())}),e.o(s)}else i&&i()}const le=typeof window<"u"?window:typeof globalThis<"u"?globalThis:global;function F(e){e&&e.c()}function L(e,s,t,i){const{fragment:a,on_mount:r,on_destroy:c,after_update:u}=e.$$;a&&a.m(s,t),i||I(()=>{const v=r.map(D).filter(V);c?c.push(...v):E(v),e.$$.on_mount=[]}),u.forEach(I)}function T(e,s){const t=e.$$;t.fragment!==null&&(E(t.on_destroy),t.fragment&&t.fragment.d(s),t.on_destroy=t.fragment=null,t.ctx=[])}function ae(e,s){e.$$.dirty[0]===-1&&(_.push(e),ee(),e.$$.dirty.fill(0)),e.$$.dirty[s/31|0]|=1<<s%31}function W(e,s,t,i,a,r,c,u=[-1]){const v=N;S(e);const l=e.$$={fragment:null,ctx:null,props:r,update:g,not_equal:a,bound:U(),on_mount:[],on_destroy:[],on_disconnect:[],before_update:[],after_update:[],context:new Map(s.context||(v?v.$$.context:[])),callbacks:U(),dirty:u,skip_bound:!1,root:s.target||v.$$.root};c&&c(l.root);let d=!1;if(l.ctx=t?t(e,s.props||{},(h,y,...k)=>{const z=k.length?k[0]:y;return l.ctx&&a(l.ctx[h],l.ctx[h]=z)&&(!l.skip_bound&&l.bound[h]&&l.bound[h](z),d&&ae(e,h)),y}):[],l.update(),d=!0,E(l.before_update),l.fragment=i?i(l.ctx):!1,s.target){if(s.hydrate){const h=Q(s.target);l.fragment&&l.fragment.l(h),h.forEach(n)}else l.fragment&&l.fragment.c();s.intro&&P(e.$$.fragment),L(e,s.target,s.anchor,s.customElement),Y()}S(v)}class O{$destroy(){T(this,1),this.$destroy=g}$on(s,t){const i=this.$$.callbacks[s]||(this.$$.callbacks[s]=[]);return i.push(t),()=>{const a=i.indexOf(t);a!==-1&&i.splice(a,1)}}$set(s){this.$$set&&!J(s)&&(this.$$.skip_bound=!0,this.$$set(s),this.$$.skip_bound=!1)}}const{document:b}=le;function ie(e){let s,t,i,a,r,c,u,v,l,d,h,y,k,z;return{c(){s=f("meta"),t=f("meta"),i=f("meta"),a=f("meta"),r=f("meta"),c=f("link"),u=f("link"),v=f("link"),l=A(),d=f("div"),h=A(),y=f("div"),k=A(),z=f("header"),z.innerHTML=`<div class="header"><div class="jumbo green lighten-5 svelte-zynurb"><div class="row"><div class="col s12 m5 l4"><img class="jumboLogo svelte-zynurb" src="/resources/images/general/1.svg" alt="AZTECA Logo"/></div> 
+          <div class="col s12 m4 push-m3 l4 center"><ul class="social-icons svelte-zynurb"><li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" target="_blank" class="svelte-zynurb"><img src="/resources/images/general/facebook-logo.svg" alt="Facebook Logo" class="svelte-zynurb"/></a></li> 
+              <li class="svelte-zynurb"><a href="https://twitter.com/azteca_home" target="_blank" class="svelte-zynurb"><img src="/resources/images/general/twitter-logo.svg" alt="Twitter Logo" class="svelte-zynurb"/></a></li> 
+              <li class="svelte-zynurb"><a href="https://www.yelp.com/biz/azteca-anthem" target="_blank" class="svelte-zynurb"><img src="/resources/images/general/yelp-logo.svg" alt="Yelp Logo" class="svelte-zynurb"/></a></li></ul></div> 
+          <div class="col s12 l4 center callBanner svelte-zynurb"><h3 class="svelte-zynurb"><i class="material-icons svelte-zynurb">phone</i>Call Us Today</h3> 
+            <a href="tel:602-926-2021" class="svelte-zynurb">(602)-926-2021</a></div></div></div> 
+      <nav class="svelte-zynurb"><div class="nav-wrapper light-green lighten-4"><ul id="nav-mobile"><li class="light-green lighten-4 waves-effect svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices" class="svelte-zynurb">Home</a></li> 
+            <li class="light-green lighten-4 svelte-zynurb"><a class="dropdown-trigger svelte-zynurb" data-target="dropdown1" href="https://www.facebook.com/AztecaHomeServices">Pest Control</a></li> 
+            <ul id="dropdown1" class="dropdown-content svelte-zynurb"><li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Our Process</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Termite Control</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Free Inspection</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Commercial</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Why Azteca?</a></li></ul> 
+            <li class="light-green lighten-4 svelte-zynurb"><a class="dropdown-trigger svelte-zynurb" data-target="dropdown2" href="https://www.facebook.com/AztecaHomeServices">Landscaping</a></li> 
+            <ul id="dropdown2" class="dropdown-content svelte-zynurb"><li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Our Process</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">General Maintenance</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Tree Removal</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Paving</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Free Inspection</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Commercial</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Why Azteca?</a></li></ul> 
+            <li class="light-green lighten-4 svelte-zynurb"><a class="dropdown-trigger svelte-zynurb" data-target="dropdown3" href="https://www.facebook.com/AztecaHomeServices">Pool</a></li> 
+            <ul id="dropdown3" class="dropdown-content green-text svelte-zynurb"><li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Our Process</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Pool Maintenance</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Pool Repair</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Free Inspection</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Commercial</a></li> 
+              <li class="svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices/" class="svelte-zynurb">Why Azteca?</a></li></ul> 
+            <li class="light-green lighten-4 waves-effect svelte-zynurb"><a href="https://www.facebook.com/AztecaHomeServices" class="svelte-zynurb">Contact</a></li></ul></div></nav></div>`,b.title="Azteca Home Services",o(s,"charset","UTF-8"),o(t,"name","Description"),o(t,"content","Azteca Home Serives: Providing Pest Control, Pool, and Landscape services in the valley of Phoenix, AZ."),o(i,"name","viewport"),o(i,"content","width=device-width, initial-scale=1"),o(a,"charset","UTF-8"),o(r,"http-equiv","X-UA-Compatible"),o(r,"content","ie=edge"),o(c,"href","https://fonts.googleapis.com/css?family=Roboto:300,400,700"),o(c,"rel","stylesheet"),o(u,"rel","stylesheet"),o(u,"href","https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.css"),o(v,"rel","stylesheet"),o(v,"href","https://fonts.googleapis.com/icon?family=Material+Icons"),o(d,"id","fb-root"),o(y,"class","fb-customerchat"),o(y,"attribution","setup_tool"),o(y,"page_id","456900321009808")},m(m,x){w(b.head,s),w(b.head,t),w(b.head,i),w(b.head,a),w(b.head,r),w(b.head,c),w(b.head,u),w(b.head,v),p(m,l,x),p(m,d,x),p(m,h,x),p(m,y,x),p(m,k,x),p(m,z,x)},p:g,i:g,o:g,d(m){n(s),n(t),n(i),n(a),n(r),n(c),n(u),n(v),m&&n(l),m&&n(d),m&&n(h),m&&n(y),m&&n(k),m&&n(z)}}}function re(e){return window.fbAsyncInit=function(){FB.init({xfbml:!0,version:"v3.3"})},function(s,t,i){var a,r=s.getElementsByTagName(t)[0];s.getElementById(i)||(a=s.createElement(t),a.id=i,a.src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js",r.parentNode.insertBefore(a,r))}(document,"script","facebook-jssdk"),[]}class ne extends O{constructor(s){super(),W(this,s,re,ie,M,{})}}function oe(e){let s;return{c(){s=f("footer"),s.innerHTML=`<div class="container"><div class="row"><div class="col m4 s12 center"><img src="resources/images/general/1.svg" alt="AZTECA Logo" class="svelte-s2cype"/> 
+          <p class="green-text text-darken-4">3655 W Anthem Way A109 #342 Phoenix, AZ 85086</p> 
+          <p class="green-text text-darken-4">(602) 926-2021</p></div> 
+        <div class="col m4 s12 center footerLogo"><img src="resources/images/general/veteranOwned.svg" alt="Azteca is veteran owned" class="svelte-s2cype"/></div> 
+        <div class="col m4 s12 center"><img src="resources/images/general/bbb.svg" alt="Azteca A+ Rating Logo" class="svelte-s2cype"/></div> 
+        <div class="col s12 center"><ul class="svelte-s2cype"><li class="svelte-s2cype"><a class="green-text text-darken-3" href="#!">Home</a></li> 
+            <li class="svelte-s2cype"><a class="green-text text-darken-3" href="#!">Pest Control</a></li> 
+            <li class="svelte-s2cype"><a class="green-text text-darken-3" href="#!">Landscaping</a></li> 
+            <li class="svelte-s2cype"><a class="green-text text-darken-3" href="#!">Pool</a></li> 
+            <li class="svelte-s2cype"><a class="green-text text-darken-3" href="#!">Contact</a></li></ul></div></div></div> 
+    <div class="footer-copyright svelte-s2cype"><div class="container">\xA9 2022 Copyright AZTECA Home Services. All rights reserved.</div></div>`,o(s,"class","page-footer light-green lighten-5 svelte-s2cype")},m(t,i){p(t,s,i)},p:g,i:g,o:g,d(t){t&&n(s)}}}class ce extends O{constructor(s){super(),W(this,s,null,oe,M,{})}}function ve(e){let s;return{c(){s=f("div"),s.innerHTML=`<h3 class="text-center text-white text-sm italic underline">Eight Annual</h3> 
+    <h1 class="text-blue-50 mt-[-1px] text-4xl center font-bold">Water Drive for the Homeless</h1> 
+    <div class="w-full flex justify-center"><p class="text-white text-2xl center max-w-[500px]">Drop off donations at Rhapsody Music, AZTECA will distribute to Phoenix
+        Rescue Mission. You can also donate money at the link below:</p></div> 
+    <a href="https://paypal.me/" class="m-8 text-3xl text-white bg-blue-400 py-2 px-4 font-bold hover:bg-blue-600 rounded-full">Donate Now</a> 
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-8 items-center"><img alt="Rhapsody Music Logo" src="/resources/images/general/rhapsody.svg" class="w-full max-w-[400px] py-4"/> 
+      <img alt="Phoenix Rescue Mission Logo" src="/resources/images/general/PhoenixRescue.svg" class="w-full max-w-[400px] py-4"/></div>`,o(s,"class","bg-blue-900 min-h-[200px] p-4 flex flex-col items-center")},m(t,i){p(t,s,i)},p:g,i:g,o:g,d(t){t&&n(s)}}}class ue extends O{constructor(s){super(),W(this,s,null,ve,M,{})}}function de(e){let s,t,i,a,r,c,u,v;return s=new ne({}),i=new ue({}),u=new ce({}),{c(){F(s.$$.fragment),t=A(),F(i.$$.fragment),a=A(),r=f("main"),r.innerHTML=`<div class="row light-green lighten-4 contactBanner svelte-zvkl63"><div class="col s12 m8 center"><h4 class="green-text text-darken-2 svelte-zvkl63">Contact us for a free estimate</h4></div> 
+      <div class="col s12 m4 center"><a href="https://www.facebook.com/AztecaHomeServices/" class="waves-effect btn btn-large green darken-2 white-text svelte-zvkl63">Contact Us</a></div></div> 
+
+    <div class="container"><div class="row"><div class="col s12 l9 row"><h4 class="mainTitle svelte-zvkl63">Azteca Home Services</h4> 
+          <img class="col s12 m6 push-m6" src="resources/images/landscape/grass5.jpg" alt="Azteca Employees Working"/> 
+          <p class="flow-text col s12 m6 pull-m6">Azteca Home Services is a full-service company that offers a wide
+            variety of services to help maintain the necessities of your home
+            and business. We service residential and commercial properties all
+            over the valley. We offer individual services such as pest control,
+            landscape services, and pool maintenance and repair. For those that
+            need full services, we have made it convenient for our clients to
+            bundle your services and save you money. You will be able to rely on
+            us to handle all of these services during your scheduled
+            appointment.</p></div> 
+        <div class="col s12 l3 center sideBanner"><h5>WHY AZTECA?</h5> 
+          <ul class="svelte-zvkl63"><li class="svelte-zvkl63">Dedicated to Quality, Workmanship and Service</li> 
+            <li class="svelte-zvkl63">Licensed, Bonded and Insured</li> 
+            <li class="svelte-zvkl63">100% Veteran Owned Business</li> 
+            <li class="svelte-zvkl63">Best in Industry Service / Customer Care Practices</li> 
+            <li class="svelte-zvkl63">Azteca is a nominee with Ranking Arizona as one of the Top
+              Family-Owned Businesses in the State of Arizona.</li></ul> 
+          <hr/> 
+          <h5>HOURS</h5> 
+          <p>Mon-Fri: 7:00AM - 7:00PM <br/>
+            Sat: 9:00AM - 5:00PM <br/>
+            Sun: CLOSED</p> 
+          <hr/> 
+          <h5>TESTIMONIALS</h5> 
+          <i class="material-icons">star</i> 
+          <i class="material-icons">star</i> 
+          <i class="material-icons">star</i> 
+          <i class="material-icons">star</i> 
+          <i class="material-icons">star</i> 
+          <blockquote class="svelte-zvkl63">&quot;Wonderful customer service and awesome techs. They make sure to
+            address all of your concerns and make sure they do all they can to
+            help. Will never use a different company again.&quot; - Ashley L.</blockquote></div> 
+        <div class="col s12"><hr/> 
+          <h4>Our Services</h4> 
+          <ul class="collapsible flow-text svelte-zvkl63"><li class="active svelte-zvkl63"><div class="collapsible-header">Residential and Commercial Pest Control</div> 
+              <div class="collapsible-body">Whether your need is small or large, Azteca can work for your
+                home or business. Our residential process includes a six-step
+                plan to get rid of your pests for good! To learn more about our
+                pest control process and pricing, visit our
+                <a href="https://facebook.com/AztecaHomeServices/">Pest Control Page</a>.</div></li> 
+            <li class="svelte-zvkl63"><div class="collapsible-header">Realtor Home Inspections</div> 
+              <div class="collapsible-body">We offer FREE inspections and estimates for realtors interested
+                in our services and offer very competitive pricing.
+                <a href="https://www.facebook.com/AztecaHomeServices/">Contact us</a>
+                about your free estimate.</div></li> 
+            <li class="svelte-zvkl63"><div class="collapsible-header">Residential and Commercial Landscape Services</div> 
+              <div class="collapsible-body">We offer general maintenance, tree removal, weed control,
+                shaping, reseeding and more! Whatever you need to keep your
+                yard, lawn, or commercial landscaping in shape, we can do it!
+                Please visit our
+                <a href="https://facebook.com/AztecaHomeServices/">Landscaping Page</a>
+                for more information about a our services.</div></li> 
+            <li class="svelte-zvkl63"><div class="collapsible-header">Pool Services</div> 
+              <div class="collapsible-body">Maintenance &amp; Repair: general maintenance, filter cleaning,
+                pool acid washes, tile cleaning, and more! Just visit our
+                <a href="https://facebook.com/AztecaHomeServices/">Pool Services</a>
+                page to learn more about what we offer.</div></li></ul></div></div></div> 
+
+    <div class="row citiesList white-text flow-text svelte-zvkl63"><div class="col s12 center"><h4 class="svelte-zvkl63">Areas We Serve</h4></div> 
+      <div class="col s12 m3 center"><ul class="svelte-zvkl63"><li class="svelte-zvkl63">Anthem</li> 
+          <li class="svelte-zvkl63">El Mirage</li> 
+          <li class="svelte-zvkl63">New River</li> 
+          <li class="svelte-zvkl63">Scottsdale</li></ul></div> 
+      <div class="col s12 m3 center"><ul class="svelte-zvkl63"><li class="svelte-zvkl63">Black Canyon City</li> 
+          <li class="svelte-zvkl63">Glendale</li> 
+          <li class="svelte-zvkl63">Pardise Valley</li> 
+          <li class="svelte-zvkl63">Sun City</li></ul></div> 
+      <div class="col s12 m3 center"><ul class="svelte-zvkl63"><li class="svelte-zvkl63">Glendale</li> 
+          <li class="svelte-zvkl63">Luke AFB</li> 
+          <li class="svelte-zvkl63">Peoria</li> 
+          <li class="svelte-zvkl63">Sun City West</li></ul></div> 
+      <div class="col s12 m3 center"><ul class="svelte-zvkl63"><li class="svelte-zvkl63">Cave Creek</li> 
+          <li class="svelte-zvkl63">Morristown</li> 
+          <li class="svelte-zvkl63">Phoenix</li> 
+          <li class="svelte-zvkl63">Surprise</li> 
+          <li class="svelte-zvkl63">Youngtown</li></ul></div></div>`,c=A(),F(u.$$.fragment)},m(l,d){L(s,l,d),p(l,t,d),L(i,l,d),p(l,a,d),p(l,r,d),p(l,c,d),L(u,l,d),v=!0},p:g,i(l){v||(P(s.$$.fragment,l),P(i.$$.fragment,l),P(u.$$.fragment,l),v=!0)},o(l){B(s.$$.fragment,l),B(i.$$.fragment,l),B(u.$$.fragment,l),v=!1},d(l){T(s,l),l&&n(t),T(i,l),l&&n(a),l&&n(r),l&&n(c),T(u,l)}}}class fe extends O{constructor(s){super(),W(this,s,null,de,M,{})}}new fe({target:document.getElementById("app")});
