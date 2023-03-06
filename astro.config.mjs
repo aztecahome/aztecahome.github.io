5ec219e0250f0ba7b6a5db102d83aec919362604
Xavier Ruiz <xruiz@bu.edu>
2023-03-06 15:10:19 -0700

    skedoosh

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import { astroImageTools } from "astro-imagetools";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), astroImageTools, compress()],
+  // build: {
+  //   // assets: "",
+  // },
+  // vite: {
+  //   build: {
+  //     assetsDir: "_astro", // if you uncomment this line, you have to make astro assets also "poop"
+  //   },
+  // },
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{svelte,js,ts}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
@@ -29,6 +29,13 @@ module.exports = {
           900: "#2F2F06",
         },
       },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
   },
   plugins: [],
