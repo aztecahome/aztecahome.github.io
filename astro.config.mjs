2502d6a0cccc612f4f1f0036de2e66e31eb68cd2
xav-ie <github@xav.ie>
2024-07-03 05:01:52 -0400

    chore(update): packages
    Also use new deps because old ones are just now working. The new
    ones work better, too. Also add some improved loading tricks.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
-import { astroImageTools } from "astro-imagetools";
+import compress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, compress({ img: false })],
-  // build: {
-  //   // assets: "",
-  // },
-  // vite: {
-  //   build: {
-  //     assetsDir: "_astro", // if you uncomment this line, you have to make astro assets also "poop"
-  //   },
-  // },
+  integrations: [tailwind(), compress({ Image: false })],
 });
