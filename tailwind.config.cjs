ed285c08c2334d6c6a31fcd08b12b885126f2fb2
Xavier Ruiz <github@xav.ie>
2026-04-16 00:05:46 -0400

    fix(container): and bounds

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1200px",
+        "2xl": "1200px",
+      },
+    },
     extend: {
       colors: {
         brown: {
